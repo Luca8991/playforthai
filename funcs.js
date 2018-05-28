1decd02713970b3d177270a70607c71301714f6f
Luca8991 <luca.berte98@hotmail.it>
2018-05-28 17:02:57 +0200

    Creazione file funcs.js (aggregazione funzioni javascript)

---
+++ funcs.js
@@ -0,0 +1,94 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+  // // The Firebase SDK is initialized and available here!
+  //
+  // firebase.auth().onAuthStateChanged(user => { });
+  // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
+  // firebase.messaging().requestPermission().then(() => { });
+  // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
+  //
+  // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+
+  try {
+    let app = firebase.app();
+    let features = ['auth', 'database', 'messaging', 'storage'].filter(feature => typeof app[feature] === 'function');
+    document.getElementById('load').innerHTML = `Firebase SDK loaded with ${features.join(', ')}`;
+  } catch (e) {
+    console.error(e);
+    document.getElementById('load').innerHTML = 'Error loading the Firebase SDK, check the console.';
+  }
+
+  $("#btn-iscrizione-squadra").click(function(){
+    var nome = $("#input-nome-squadra").val().toUpperCase();
+    var girone = $("#select-girone").val();
+
+    var teamData = {
+        name: nome,
+        girone: girone,
+        score: 0,
+        gf: 0,
+        gs: 0
+      };
+
+    // Get a key for a new Post.
+    var newKey = firebase.database().ref().push().key;
+
+    // Write the new post's data simultaneously in the posts list and the user's post list.
+    var updates = {};
+    updates['/squadre/' + newKey] = teamData;
+
+    firebase.database().ref().update(updates, completed);
+  });
+
+  $("#select-girone-sfide").on('change', function(){
+    var girone = this.value;
+    firebase.database().ref('/squadre').orderByChild('girone').equalTo(girone).once('value', function(snap){
+      //$("#select-squadre option[value='0']").remove();
+      $("#select-squadra-1").find("option").remove();
+      $("#select-squadra-2").find("option").remove();
+
+      var i = snap.numChildren();
+      //$("#numero-squadre span").text(i);
+      snap.forEach(function (snapshot) {
+        var key = snapshot.key;
+        var name = snapshot.val().name;
+
+        $('#select-squadra-1').append($("<option></option>").attr("value",key).text(name));
+        $('#select-squadra-2').append($("<option></option>").attr("value",key).text(name));
+
+        i--;
+      });
+
+      //$('#select-squadre').append($("<option></option>").attr("value",0).text("0 - 0"));
+    });
+  });
+
+  $("#btn-insert-sfida").click(function(){
+    var andata = $("#input-ora-sfida-and").val();
+    var ritorno = $("#input-ora-sfida-rit").val();
+    var s1 = $("#select-squadra-1").val();
+    var s2 = $("#select-squadra-2").val();
+
+    var sfidaData = {
+        s1: s1,
+        s2: s2,
+        andata: andata,
+        ritorno: ritorno,
+        risultato: 0
+      };
+
+    // Get a key for a new Post.
+    var newKey = firebase.database().ref().push().key;
+
+    // Write the new post's data simultaneously in the posts list and the user's post list.
+    var updates = {};
+    updates['/partite/' + newKey] = sfidaData;
+
+    firebase.database().ref().update(updates, completed);
+  });
+
+});
+
+function completed(){
+  alert("Fatto!");
+}
